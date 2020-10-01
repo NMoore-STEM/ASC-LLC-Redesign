33258aef86b344c743611f1e269947255b08cdac
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2020-10-01 17:14:38 -0600

    Added JQuery to script in HTML head and created JS file - starting work on scroll down header reveal...

---
+++ script.js
@@ -0,0 +1,9 @@
+//Reveal header for MOBILE and TABLET//
+$(document).ready(function(){
+    $(document).scroll(function(){
+        if ($(document).scrollTop() > 450) {
+            $('#headerMT').addClass('show');
+        } else {
+            $('#headerMT').removeClass('show');
+        }
+    });
