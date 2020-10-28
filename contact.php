694e5de28bf5d7a5f32741ddf527da7cddd9e41f
NMoore-STEM <66744927+NMoore-STEM@users.noreply.github.com>
2020-10-28 12:54:14 -0600

    More attempts to get email service set up for ASC contact form - scrapped old approach - using guidance from hosting service website - still need to configure DNS service for domain (NameCheap)

---
+++ contact.php
@@ -0,0 +1,111 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->IsHTML(true);
+    //for debugging
+    $mail->SMTPDebug = 2;
+
+    //below code might not be needed
+    $mail->Subject = "Subject Text";
+    $mail->Body = "<i>Mail body in HTML</i>";
+    $mail->AltBody = "This is the plain text version of the email content";
+
+    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+
+    if(empty($_POST['name'])  || 
+       empty($_POST['email']) || 
+       empty($_POST['message']))
+    {
+    $errors .= "\n Error: all fields are required";
+    }
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $date = date('Y-m-d');
+
+    $formcontent= "From: $name \n Email: $email \n Message: $message \n";
+    $recipient = "nick@anandascience.com";
+    $subject = "Client Message from Webpage Contact Form";
+    $mailheader = "From: $email \r\n Date: $date \r\n";
+
+    //first code to verify email format input
+    function shapeSpace_check_email($email) {
+        return preg_match('#^[a-z0-9.!\#$%&\'*+-/=?^_`{|}~]+@([0-9.]+|([^\s]+\.+[a-z]{2,6}))$#si', $email);
+    }
+    //second code to verify email format input
+    filter_var($email, FILTER_VALIDATE_EMAIL);
+    //third email field verification
+    if (!preg_match(
+        "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+        $email_address))
+        {
+            $errors .= "\n Error: Invalid email address";
+        }
+    //check for new line - injection attack security
+    function shapeSpace_check_newlines($string) {
+        if (preg_match("/(%0A|%0D|\\n+|\\r+)/i", $string) != 0) {
+            return false;
+        }
+        return true;
+    }
+    // Protect against header injection attacks
+    function check_header_injection($fields) {
+	    $injection = false;
+	    for ($n = 0; $n < count($fields); $n++) {
+		if (eregi("%0A", $fields[$n]) || eregi("%0D", $fields[$n]) || eregi("\r", $fields[$n]) || eregi("\n", $fields[$n])) {
+			$injection = true;
+		    }
+	    }
+	    return $injection;
+    }
+	
+$from    = $_POST['from'];
+$email   = $_POST['email'];
+$subject = $_POST['subject'];
+
+$result = check_header_injection(array($from, $email, $subject));
+
+if ($result == true) die();
+
+    //verify POST method for sending
+    function shapeSpace_check_post_request() {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+            return false;
+        }
+        return true;
+    }
+    // Strip HTML entities
+function strip_html_entities($str) {
+	return preg_replace("/&[a-z0-9]{2,6}+;/i", '', $str);
+}
+
+    if (stristr($str, '&'))) $str = strip_html_entities($str);
+
+    try {
+        $mail->send();
+        echo "Message has been sent successfully";
+    } catch (Exception $e) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+
+    //security measure against spamBots
+    if(!empty($_POST['botCatfish'])) {
+        // it's spam
+    } else {
+        // it's human
+    }
+
+    //NEEDS MORE WORK BELOW
+    echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+
+return mail($recipient, $subject, $message, $headers);
+
+?>
